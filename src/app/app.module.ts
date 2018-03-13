b93f9d4733126d4e85ce17e5edf17b3438ef0a96
yejunjie <yejunjie@feitaikeji.com>
2018-03-13 09:46:44 +0800

    ceshi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,40 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule,Routes} from '@angular/router';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+
+const appRoutes:Routes=[
+	{
+		path:'',redirectTo:'main',pathMatch:'full'
+	},
+	{
+		path:'login',component:LoginComponent
+	}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NgZorroAntdModule.forRoot()
+    NgZorroAntdModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   bootstrap: [AppComponent]
 })
